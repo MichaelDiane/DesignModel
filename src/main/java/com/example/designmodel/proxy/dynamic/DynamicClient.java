a09c4c3a0aa28f4ebf40c003109893a00d344295
xiongda <michaeldiane@163.com>
2022-03-10 17:45:23 +0800

    添加

--- src/main/java/com/example/designmodel/proxy/dynamic/DynamicClient.java
+++ src/main/java/com/example/designmodel/proxy/dynamic/DynamicClient.java
@@ -8,7 +8,7 @@ package com.example.designmodel.proxy.dynamic;
  * jdk动态代理与cglib动态代理对比:
      *1.jdk采用读取接口的信息,cglib是覆盖父类方法  两者目的都是 生成一个新的类,去实现增强逻辑代码的功能
      *2.jdk proxy 对于用户而言,必须要有一个接口实现,目标类相对来说复杂;而 cglib 可以代理任意一个皮同类,没有任何的要求
-     *3.jdk 生成代理逻辑简单,执行效率要不cglib低,每次均需要反射动态调用;而cglib动态代理得生成逻辑更复杂,调用效率更高.生了一个包含了所有逻辑的FastClass,不再需要反射调用
+     *3.jdk 生成代理逻辑简单,执行效率要不cglib低,每次均需要反射动态调用;而cglib动态代理得生成逻辑更复杂,调用效率更高.生了一个包含了所有逻辑的FastClass[其根源是在于在fastclass中为代理对象和被代理对象均生成了有个序index,当想调用哪个的时候直接根据序直接定位调用],不再需要反射调用
      *
  */
 public class DynamicClient {
