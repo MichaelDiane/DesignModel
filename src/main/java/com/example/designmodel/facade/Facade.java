56b193fa8a20a769ac210c2dc02cb3c1ae62f0ad
xiongda <michaeldiane@163.com>
2022-03-14 11:06:24 +0800

    添加优缺点

--- src/main/java/com/example/designmodel/facade/Facade.java
+++ src/main/java/com/example/designmodel/facade/Facade.java
@@ -4,6 +4,8 @@ package com.example.designmodel.facade;
  * @author xiongda
  * @ClassName Facade 门面
  * @Description 结构型模式-门面模式
+ *  优点: 简化调用过程,减少系统依赖,松散耦合,遵循米特法则
+ *  缺点: 增加子系统和扩展时容易带来未知风险,不符合开闭原则,某些情况下违背单一职责原则
  * @createTime 2022/3/11 17:36
  */
 public class Facade {
